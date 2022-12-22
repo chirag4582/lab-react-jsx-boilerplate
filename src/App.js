81340f906ce2bf2c72aa5fc774ea2e46d95c5cc8
chirag4851 <chiragchavda1243@gmail.com>
2022-12-22 13:53:55 +0530

    Kalvium Gallery

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
-import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
-function App() {
+function App(props) {
   // code here
+  return(
+    <div >
+      <h3 >Kalvium gallery</h3>
+      <div className='container'>
+        {
+          props.images.map((elt)=>{
+            return <img className="image" src={elt.img} alt="elephant" ></img>
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import imageData from './components/Components';
 // import AppClass from "./AppClass";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
+    <App images = {imageData()} />   
     {/* change the above line - for interchaning between App and AppClass */}
   </React.StrictMode>
 );
